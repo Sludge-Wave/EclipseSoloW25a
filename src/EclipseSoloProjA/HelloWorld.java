76bc82f7c86f483b916d59f5f516b208f7969eb7
saher <saher@164.76.37.223>
2025-02-03 12:36:19 -0500

    Programmer1 commit 3

--- src/EclipseSoloProjA/HelloWorld.java
+++ src/EclipseSoloProjA/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 1 did this");
 	}
 }
